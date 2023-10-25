75334b6ed2f0718ab851005f7e8ccd40f4319dea
vladnov138 <vladnov138@gmail.com>
2023-10-25 11:24:26 +0800

    fix hive adapter problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:task_manager/models/task.dart';
 
 import 'client/hive_names.dart';
+import 'models/task.dart';
 import 'task_manager_app.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter<Task>(TaskAdapter());
   await Hive.openBox<Task>(HiveBoxes.task);
-  Hive.registerAdapter(TaskAdapter());
   runApp(const TaskManagerApp());
 }
 
