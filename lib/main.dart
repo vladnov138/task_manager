f7b0b2e4261c5baebd7eba423305dadcc65c1741
vladnov138 <vladnov138@gmail.com>
2023-10-27 02:51:40 +0800

    add dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:task_manager/repository/abstract_repository.dart';
 import 'package:task_manager/services/NotificationService.dart';
 
 import 'client/hive_names.dart';
 import 'models/task.dart';
+import 'models/user/user.dart';
+import 'repository/repository.dart';
 import 'task_manager_app.dart';
 
 Future<void> main() async {
@@ -14,6 +18,8 @@ Future<void> main() async {
   GetIt.I.registerLazySingleton<NotificationService>(
           () => NotificationService());
   GetIt.I<NotificationService>().initilizeNotifications();
+  GetIt.I.registerLazySingleton<AbstractRepository>(
+          () => Repository(dio: Dio()));
   runApp(const TaskManagerApp());
 }
 
