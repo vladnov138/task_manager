1e5243bb787d25bcc5280e0d4eaeb7329716b9ea
vladnov138 <vladnov138@gmail.com>
2023-10-27 14:12:47 +0800

    add upload and download buttons

--- lib/features/profile/view/profile_screen.dart
+++ lib/features/profile/view/profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:task_manager/repository/abstract_repository.dart';
 import 'package:task_manager/router/router.dart';
+import 'package:task_manager/theme/theme.dart';
 
 import '../../../generated/l10n.dart';
 
@@ -11,37 +14,48 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Planning")
-      ),
+      appBar: AppBar(title: const Text("Planning")),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text(S.of(context).plans),
-              leading: const Icon(Icons.calendar_month),
-              onTap: () {
-                AutoRouter.of(context).push(const ScheduleListRoute());
-              },
-            ),
-            ListTile(
-              title: Text(S.of(context).analytics),
-              leading: const Icon(Icons.analytics_outlined),
-            ),
-            ListTile(
-              title: Text(S.of(context).settings),
-              leading: const Icon(Icons.settings),
-            ),
-            ListTile(
-              title: Text(S.of(context).logOut),
-              leading: const Icon(Icons.logout_outlined),
-              onTap: () {
-
-              },
-            ),
-          ],
-        )
-      ),
+          child: ListView(
+        children: [
+          ListTile(
+            title: Text(S.of(context).plans),
+            leading: const Icon(Icons.calendar_month),
+            onTap: () {
+              AutoRouter.of(context).push(const ScheduleListRoute());
+            },
+          ),
+          ListTile(
+            title: Text(S.of(context).analytics),
+            leading: const Icon(Icons.analytics_outlined),
+          ),
+          ListTile(
+            title: Text(S.of(context).settings),
+            leading: const Icon(Icons.settings),
+          ),
+          ListTile(
+            title: Text(S.of(context).logOut),
+            leading: const Icon(Icons.logout_outlined),
+            onTap: () {},
+          ),
+        ],
+      )),
+      body: Column(children: [
+        MaterialButton(
+          color: mainTheme.primaryColor,
+          child: Text(S.of(context).synchronize),
+          onPressed: () {
+            GetIt.I<AbstractRepository>().uploadData();
+          },
+        ),
+        MaterialButton(
+          color: mainTheme.primaryColor,
+          child: Text(S.of(context).download),
+          onPressed: () {
+            GetIt.I<AbstractRepository>().downloadData();
+          },
+        ),
+      ]),
     );
   }
 }
